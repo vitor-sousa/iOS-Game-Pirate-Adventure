c224ad9830ac752ff97cd9a95698cc9ab6330c6a
Vitor Sousa <vitorsousa@MacBook-Pro-de-Vitor.local>
2015-08-25 13:29:12 -0300

    Initial Commit

---
+++ Pirate Adventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventure
+//
+//  Created by Vitor Sousa on 25/08/15.
+//  Copyright (c) 2015 Vitor Sousa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pirate Adventure/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Pirate Adventure
+//
+//  Created by Vitor Sousa on 25/08/15.
+//  Copyright (c) 2015 Vitor Sousa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
